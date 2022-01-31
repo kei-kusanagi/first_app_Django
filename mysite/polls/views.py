628673bf855eaa2190682432e55271e9ebf13e5e
kei-kusanagi <kei_kusanagi@outlook.com>
2022-01-31 15:53:59 -0600

    se actualiza la plantilla de /polls/detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
 from .models import Question
 from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,10 +13,7 @@ def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 # def detail(request, question_id):
