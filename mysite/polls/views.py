bf6eeea4fd1c87efb526c714870940b2189dbafb
kei-kusanagi <kei_kusanagi@outlook.com>
2022-02-01 15:30:01 -0600

    se implementan mas pruebass a la polls/test.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -73,5 +73,13 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
 
 
