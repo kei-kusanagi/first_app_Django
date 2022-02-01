9856475fbae293aac9a46cc6232c7c0df110d1c0
kei-kusanagi <kei_kusanagi@outlook.com>
2022-02-01 16:51:00 -0600

    se agregan templates a la seccion de administracion

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,10 +4,19 @@ from .models import Choice, Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    model = Choice
+    extra = 3
+    list_display = ('question_text', 'pub_date')
+
+
+class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
